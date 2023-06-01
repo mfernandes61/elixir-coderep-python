d85b2ddeff068de62d871d9f20285b0e141b0b7f
mfernandes61 <mfernades61@gmail.com>
2023-06-01 17:37:30 +0100

    fixed html functions, created py script to test them & an example image

--- html_gen.py
+++ html_gen.py
@@ -3,24 +3,24 @@
 # start_html
 def start_html():
   f = open('report.html','w')
-  message = """<html><head></head><body>"""
+  message = "<html><head></head><body>"
   f.write(message)
   f.close()
 
 def html_para(text):
   f = open('report.html','a')
-  message = """<p>""" + text + """</p>""""
+  message = "<p>" + text + "</p>"
   f.write(message)
   f.close()
   
 def html_img(text, img_path):
   f = open('report.html','a')
-  message =  """<p>""" + text + """</p>"""" + """<p><img src=\""""" + img_path + """\"></p>"""
+  message =  "<p>" + text + "</p>" + "<p><img src=\"" + img_path + "\"></p>"
   f.write(message)
   f.close()
   
 def end_html():
   f = open('report.html','a')
-  message = """ </body></html>"""
+  message = "</body></html>"
   f.write(message)
   f.close()
